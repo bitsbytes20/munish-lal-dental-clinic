817bfea2da15a618c75bb36265f566208d387f52
Aman Kumar <bitsbytes2006@gmail.com>
2026-07-08 02:36:17 +0530

    seo updated

--- components/seo/local-business-schema.tsx
+++ components/seo/local-business-schema.tsx
@@ -57,23 +57,16 @@ export default function LocalBusinessSchema() {
       },
     ],
 
-    sameAs: [
-      "https://www.instagram.com/drshubhrasdentalcare/",
-      "https://www.google.com/maps/place/Dr.+Shubhra's+Dental+Clinic+%26+Implant+center/@28.3802313,79.4286145,17z/data=!3m1!4b1!4m6!3m5!1s0x39a007e27569cb39:0xefdf2e333831ddb0!8m2!3d28.3802266!4d79.4311948!16s%2Fg%2F11mll7x4sn",
-    ],
+ sameAs: [
+  "https://www.instagram.com/drshubhrasdentalcare/",
+  "https://maps.app.goo.gl/i1v37R17ALEkwCoP7"
+],
 
-    hasMap:
-      "https://www.google.com/maps/place/Dr.+Shubhra's+Dental+Clinic+%26+Implant+center/",
+hasMap: "https://maps.app.goo.gl/i1v37R17ALEkwCoP7",
 
-    medicalSpecialty: [
-      "General Dentistry",
-      "Cosmetic Dentistry",
-      "Dental Implants",
-      "Root Canal Therapy",
-      "Teeth Whitening",
-      "Orthodontics",
-      "Pediatric Dentistry",
-    ],
+    
+
+   
   };
 
   return (
